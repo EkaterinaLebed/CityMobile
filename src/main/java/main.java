67f68246cb3614e9ea43faaccadd09d46167ab1a
Ekaterina Lebed <my@mail.com>
2017-07-28 03:00:07 +0300

    page "Create abonent"

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -14,25 +14,6 @@ public class main {
     public static void main(String[] args){
         CustomerDao customerDao = new CustomerDaoImpl();
         Customer customer = customerDao.read(5);
-        System.out.println("*OK*"+customer);
-
-        System.out.println("------------------");
-        Customer customer1 = new Customer();
-        customer1.setName("000");
-        customerDao.create(customer1);
-        System.out.println(customerDao.selectAll());
-
-        System.out.println("------------------");
-        BillDao billDao= new BillDaoImpl();
-        List<Bill> list = billDao.selectByPeriod(new GregorianCalendar(2017, 0, 1).getTime(), new GregorianCalendar(2017, 11, 31).getTime());
-        System.out.println(list);
-
-        System.out.println("------------------");
-        PaymentDao paymentDao= new PaymentDaoImpl();
-        List<Payment> list2 = paymentDao.selectByPeriod(new GregorianCalendar(2017, 0, 1).getTime(), new GregorianCalendar(2017, 11, 31).getTime());
-        System.out.println(list2);
-        System.out.println("------------------");
-
         AppSessionFactory.close();
     }
 
