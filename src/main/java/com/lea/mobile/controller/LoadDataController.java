860f1e37ee0c5f24af35645d67a8d483b8c65ef6
Ekaterina Lebed <my@mail.com>
2017-10-23 02:21:54 +0300

    change load data controller

---
+++ src/main/java/com/lea/mobile/controller/LoadDataController.java
@@ -0,0 +1,15 @@
+package com.lea.mobile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping({"/loaddata","load"})
+public class LoadDataController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView  showBillingPage(){
+        return new ModelAndView("pages/loadData");
+    }
+}
